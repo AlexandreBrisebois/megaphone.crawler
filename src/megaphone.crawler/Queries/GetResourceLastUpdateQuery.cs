c0ab0e39b114ac90071cb00d2f46c6cfa273d090
Alexandre Brisebois <brisebois@outlook.com>
2021-03-20 15:56:20 -0400

    update

--- src/megaphone.crawler/Queries/GetResourceLastUpdateQuery.cs
+++ src/megaphone.crawler/Queries/GetResourceLastUpdateQuery.cs
@@ -11,15 +11,18 @@ namespace Megaphone.Crawler.Queries
     internal class GetResourceLastUpdateQuery : IQuery<DaprClient, ResourceLastUpdateRepresentation>
     {
         private readonly Guid id;
+        private readonly string host;
 
         public GetResourceLastUpdateQuery(string url)
         {
-            id = url.ToUri().ToGuid();
+            var uri = url.ToUri();
+            id = uri.ToGuid();
+            host = uri.Host;
         }
 
         public async Task<ResourceLastUpdateRepresentation> ExecuteAsync(DaprClient model)
         {
-            var response = await model.InvokeMethodAsync<ResourceLastUpdateRepresentation>(HttpMethod.Get, "resources", $"api/resources/{id}/last-updated");
+            var response = await model.InvokeMethodAsync<ResourceLastUpdateRepresentation>(HttpMethod.Get, "resources", $"api/resources/{host}/{id}/last-updated");
             return response;
         }
     }
